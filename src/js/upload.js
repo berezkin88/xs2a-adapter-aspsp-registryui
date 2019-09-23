c3a2c0e8a0f9d8d63828258bbc435a901178ae9e
Oleksandr Berezkin <oleksandrberezkin@Oleksandrs-MacBook-Pro.local>
2019-09-23 16:12:38 +0300

    adjust upload method

--- src/js/upload.js
+++ src/js/upload.js
@@ -2,7 +2,7 @@ function upload() {
     let file = FILE_UPLOAD_FIELD.files[0];
     let data = new FormData();
 
-    data.append("data", file);
+    data.append("file", file);
 
     fetch("/v1/aspsps/import", {
         method: 'POST',
