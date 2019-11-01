26272215aa2c1640f2e13be127c7014834d36854
berezkin88 <berezkin05@gmail.com>
2019-11-01 12:00:22 +0200

    exportButton() sends POST requests now

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -165,7 +165,9 @@ function importButton() {
 }
 
 function exportButton() {
-    fetch("/v1/aspsps/outbound/export").then(response => {
+    fetch("/v1/aspsps/outbound/export", {
+        method: 'POST'
+    }).then(response => {
         if (!response.ok) {
             throw Error(response.statusText);
         }
